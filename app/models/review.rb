9aedde0313cce3da004096095f3dfae74d391f90
samuelmaker <samuelwood.co@gmail.com>
2018-02-21 11:29:51 +0000

    added validation to reviewsq

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,4 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  validates :rating, presence: true, numericality:{only_integer: true, greater_than: 0, less_than: 11, message: "- Please enter a rating between 1 and 10."}
 end
