3a1625e9db0a46689bfd6f0a85426d78f3092226
Benjamin Pourian <b.pourian@hotmail.co.uk>
2018-02-22 14:53:18 +0000

    update to user class

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,6 @@
 class Review < ApplicationRecord
+  belongs_to :user
   belongs_to :restaurant
+
   validates :rating, presence: true, numericality:{only_integer: true, greater_than: 0, less_than: 11, message: "- Please enter a rating between 1 and 10."}
 end
