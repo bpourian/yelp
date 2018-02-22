17a7ef70ad46216fabb02cab48fad4816e7c69e0
samuelmaker <samuelwood.co@gmail.com>
2018-02-22 14:38:22 +0000

    incomplete review association with user

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,7 @@
 class Review < ApplicationRecord
   belongs_to :restaurant
+  belongs_to :user
+
+
   validates :rating, presence: true, numericality:{only_integer: true, greater_than: 0, less_than: 11, message: "- Please enter a rating between 1 and 10."}
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   has_many :restaurants
-  
+  has_many :reviews
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
