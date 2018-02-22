e41886ec495a3c5d8f0a14e43e8cd6c50fba3d5b
Leigh-ann <leighann.gant@gmail.com>
2018-02-22 17:44:47 +0000

    rspec set up - feature test added

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,27 +1,38 @@
 class ReviewsController < ApplicationController
   before_action :set_review, only: [:show, :edit, :update, :destroy]
 
+  # GET /reviews
+  # GET /reviews.json
   def index
     @reviews = Review.all
   end
 
+  # GET /reviews/1
+  # GET /reviews/1.json
   def show
   end
 
+  # GET /reviews/new
   def new
     @review = Review.new
   end
 
+  # GET /reviews/1/edit
   def edit
   end
 
+  # POST /reviews
+  # POST /reviews.json
   def create
     @restaurant = Restaurant.find(params[:restaurant_id])
     @review = @restaurant.reviews.new(review_params)
     @review.user_id = current_user.id
+    @review.save
     redirect_to @restaurant
   end
 
+  # PATCH/PUT /reviews/1
+  # PATCH/PUT /reviews/1.json
   def update
     respond_to do |format|
       if @review.update(review_params)
---
+++ spec/feature_test/add_review_spec.rb
@@ -0,0 +1,26 @@
+require 'rails_helper'
+
+RSpec.describe "Add review", :type => :feature do
+  before do
+    user = User.create email: 'tansaku@gmail.com', password: '12345678', password_confirmation: '12345678'
+    login_as user
+  end
+  feature 'Create Review' do
+    scenario 'create one review for a restaurant' do
+      add_restaurant
+      fill_in 'review_rating', with: 5
+      fill_in 'review_comment', with: 'Really Good!!'
+      click_button 'Create Review'
+      within('div.reviewrating') {expect(page).to have_content('5')}
+      within('div.reviewcomment') {expect(page).to have_content('Really Good!!')}
+    end
+
+    scenario 'create one review for a restaurant and check the reviewer is displayed' do
+      sign_up(email='bob@bobsbananas.com')
+      create_restaurant('Testaurant', 'Delicious')
+      create_review(2, 'really good!!')
+      within('div#reviews') {expect(page).to have_content('bob@bobsbananas.com')}
+    end
+
+  end
+end
