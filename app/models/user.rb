e9d549705a135492d5fff4e614c585df3f7cc705
GiadaSimonetti <21024993+GiadaSimonetti@users.noreply.github.com>
2018-02-23 11:46:25 +0000

    Add facebook signup

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,16 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  devise :omniauthable, :omniauth_providers => [:facebook]
+
+  def self.find_for_facebook_oauth(auth)
+  where(auth.slice(:provider, :uid)).first_or_create do |user|
+    user.provider = auth.provider
+    user.uid = auth.uid
+    user.email = auth.info.email
+    user.password = Devise.friendly_token[0,20]
+    #user.name = auth.info.name # no name by default
+    #user.image = auth.info.image # no image by default
+    end
+  end
 end
--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -276,4 +276,6 @@ Devise.setup do |config|
   # When using OmniAuth, Devise cannot automatically set OmniAuth path,
   # so you need to do it manually. For the users scope, it would be:
   # config.omniauth_path_prefix = '/my_engine/users/auth'
+  require "omniauth-facebook"
+  config.omniauth :facebook, ENV['app_id'], ENV['secret_key']
 end
